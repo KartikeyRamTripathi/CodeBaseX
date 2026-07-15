5612bd263f0b8922e09dd94c3f7fed0a9f377d82
KartikeyRamTripathi <tripathikartikey883@gmail.com>
2026-07-15 23:50:09 +0530

    Add files via upload

---
+++ loops/ReverseAlphabetPattern.java
@@ -0,0 +1,18 @@
+public class ReverseAlphabetPattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+
+            char ch = (char) ('A' + (n - i - 1));
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+
+            System.out.println();
+        }
+    }
+}
