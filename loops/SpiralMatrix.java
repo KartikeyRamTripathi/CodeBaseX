223bd1db7e3537536df835fd6f13f3ee9ab96709
KartikeyRamTripathi <tripathikartikey883@gmail.com>
2026-07-24 23:10:46 +0530

    Add files via upload

---
+++ loops/SpiralMatrix.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SpiralMatrix {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size (n) of the spiral matrix : ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = num++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = num++;
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    matrix[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    matrix[i][left] = num++;
+                }
+                left++;
+            }
+        }
+
+        System.out.println("\nSpiral Matrix:");
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.printf("%4d", val);
+            }
+            System.out.println();
+        }
+    }
+}
